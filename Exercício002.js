1bc2969fe012e5cdf4df2d3567f029c390d3a07c
Gabiru-IF <gabriel.moreira@escolar.ifrn.edu.br>
2025-05-12 11:52:39 -0300

    Continuação Ex2

--- Exercício002.js
+++ Exercício002.js
@@ -1,11 +1,13 @@
 let listaProdutos = []
+var emAlteração = false
+var indiceElementoAlteracao = -1
 
-function gravarProduto(){
-
+function gravarProduto(event){
+    event.preventDefault()
     let nomeProduto = document.getElementById('nomeProdutos').value
     let qtdEstoque = document.getElementById('qtdEstoque').value
     let valorUnitario = document.getElementById('valorUnitario').value
-    let fornecedor = document.getElementById('forcenedor').value
+    let fornecedor = document.getElementById('fornecedor').value
 
     let produto = {
 
@@ -16,7 +18,15 @@ function gravarProduto(){
 
     }
 
-    listaProdutos.push(produto)
+    if (emAlteração) {
+        listaProdutos[indiceElementoAlteracao] = produto 
+        emAlteração = false
+        indiceElementoAlteracao = -1
+    }else{
+        listaProdutos.push(produto)
+    }
+
+    limpar()
     listarProdutos()
 
 }
@@ -32,10 +42,13 @@ function listarProdutos() {
 
             conteudo += `
                 <tr>
+                    <td>${indice}</td>
                     <td>${produto.nomeProduto}</td>
                     <td>${produto.qtdEstoque}</td>
-                    <td>${produto.valorUnitario}</td>
+                    <td>R$${parseFloat(produto.valorUnitario).toFixed(2)}</td>
                     <td>${produto.fornecedor}</td>
+                    <td><button type="button" onclick="alterar(${indice})">Alterar</button></td>
+                    <td><button type="button" onclick="excluir(${indice})">Excluir</button></td>
                 </tr>
             `
 
@@ -46,4 +59,27 @@ function listarProdutos() {
     document.getElementById('conteudo').innerHTML = conteudo
 
 
+}
+
+function excluir(indice){
+
+    if (confirm(`Deseja Realmente Excluir este Produto ${listaProdutos[indice].nomeProduto}?`)){
+        listaProdutos.splice(indice, 1)
+        listarProdutos()
+    }
+}
+
+function alterar(indice){
+    emAlteração = true
+    indiceElementoAlteracao = indice
+    document.getElementById('nomeProdutos').value = listaProdutos[indice].nomeProduto
+    document.getElementById('qtdEstoque').value = listaProdutos[indice].qtdEstoque
+    document.getElementById('valorUnitario'). value = listaProdutos[indice].valorUnitario
+    document.getElementById('fornecedor').value = listaProdutos[indice].fornecedor
+    gravarProduto()
+}
+
+function limpar(){
+    document.getElementById('meuForm').reset()
+    emAlteração = false
 }
