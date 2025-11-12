29050e677e7116cb1b17063ccbfb6c824c22f89a
Gabiru-IF <gabriel.moreira@escolar.ifrn.edu.br>
2025-11-12 10:35:13 +0000

    inicioprovab3

---
+++ ProvaB3-Angular/src/app/app.ts
@@ -0,0 +1,58 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('ProvaB3-Angular');
+
+  combustibleSelecionado = '';
+  valorLitro = 0;
+  quantidadeLitros = 0;
+  dataAbastecimento = '';
+
+  totalCalculado = 0;
+  mostrarResultado = false;
+  registroAbastecimento: any[] = [];
+
+  registrarAbastecimento() {
+    if (!this.combustibleSelecionado) {
+      alert('Por favor, selecione um tipo de combustível!');
+      return;
+    }
+
+    if (this.valorLitro <= 0 || this.quantidadeLitros <= 0) {
+      alert('Valor por litro e quantidade de litros devem ser maiores que zero!');
+      return;
+    }
+
+
+    this.totalCalculado = this.valorLitro * this.quantidadeLitros;
+    this.mostrarResultado = true;
+
+    const registro = {
+      combustivel: this.combustibleSelecionado,
+      valorLitro: this.valorLitro,
+      quantidadeLitros: this.quantidadeLitros,
+      data: this.dataAbastecimento,
+      total: this.totalCalculado
+    };
+    this.registroAbastecimento.push(registro);
+
+    this.limparFormulario();
+  }
+
+  limparFormulario() {
+    this.combustibleSelecionado = '';
+    this.valorLitro = 0;
+    this.quantidadeLitros = 0;
+    this.dataAbastecimento = '';
+  }
+}
